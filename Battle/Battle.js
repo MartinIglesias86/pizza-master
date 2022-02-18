bc2deb5884aa3ad8e1b79e593450acfb939ff871
Martin Iglesias <72715575+MartinIglesias86@users.noreply.github.com>
2022-02-18 20:05:33 -0300

    images

--- Battle/Battle.js
+++ Battle/Battle.js
@@ -91,7 +91,7 @@ class Battle {
         this.element.classList.add("Battle");
         this.element.innerHTML = (`
         <div class="Battle_hero">
-            <img src="${'images/characters/people/hero.png'}" alt="Hero" />
+            <img src="${'/images/characters/people/hero.png'}" alt="Hero" />
         </div>
         <div class="Battle_enemy">
             <img src=${this.enemy.src} alt=${this.enemy.name} />
--- OverworldMap.js
+++ OverworldMap.js
@@ -107,8 +107,8 @@ class OverworldMap {
 window.OverworldMaps = {
     DemoRoom: {
         id: "DemorRoom",
-        lowerSrc: "images/maps/DemoLower.png",
-        upperSrc: "images/maps/DemoUpper.png",
+        lowerSrc: "/images/maps/DemoLower.png",
+        upperSrc: "/images/maps/DemoUpper.png",
         gameObjects: {
             hero: new Person({
                 isPlayerControlled: true,
@@ -118,7 +118,7 @@ window.OverworldMaps = {
             npcA: new Person({
                 x: utils.withGrid(7),
                 y: utils.withGrid(9),
-                src: "images/characters/people/npc1.png",
+                src: "/images/characters/people/npc1.png",
                 behaviorLoop: [
                     {type: "stand", direction: "left", time: 800},
                     {type: "stand", direction: "up", time: 1000},
@@ -147,7 +147,7 @@ window.OverworldMaps = {
             npcB: new Person({
                 x: utils.withGrid(8),
                 y: utils.withGrid(5),
-                src: "images/characters/people/erio.png",
+                src: "/images/characters/people/erio.png",
                 talking: [{
                     events: [
                         { type: "textMessage", text: "Los jovenes de hoy no quieren trabajar!!!", faceHero: "npcB" },
@@ -228,8 +228,8 @@ window.OverworldMaps = {
     },
     Kitchen: {
         id: "Kitchen",
-        lowerSrc: "images/maps/KitchenLower.png",
-        upperSrc: "images/maps/KitchenUpper.png",
+        lowerSrc: "/images/maps/KitchenLower.png",
+        upperSrc: "/images/maps/KitchenUpper.png",
         gameObjects: {
             hero: new Person({
                 isPlayerControlled: true,
@@ -239,7 +239,7 @@ window.OverworldMaps = {
             npcB: new Person({
                 x: utils.withGrid(10),
                 y: utils.withGrid(8),
-                src: "images/characters/people/npc3.png",
+                src: "/images/characters/people/npc3.png",
                 talking: [
                     {
                         events: [
@@ -267,8 +267,8 @@ window.OverworldMaps = {
     },
     Street: {
         id: "Street",
-        lowerSrc: "images/maps/StreetLower.png",
-        upperSrc: "images/maps/StreetUpper.png",
+        lowerSrc: "/images/maps/StreetLower.png",
+        upperSrc: "/images/maps/StreetUpper.png",
         gameObjects: {
             hero: new Person({
             isPlayerControlled: true,
