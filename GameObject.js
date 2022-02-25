cd113aa97affd79472c54662a4c14c8a5221477e
Martin Iglesias <72715575+MartinIglesias86@users.noreply.github.com>
2022-02-25 11:12:16 -0300

    optimizing  code

--- GameObject.js
+++ GameObject.js
@@ -7,7 +7,7 @@ class GameObject {
         this.direction = config.direction || "down";
         this.sprite = new Sprite ({
             gameObject: this,
-            src: config.src || "../images/characters/people/hero.png",
+            src: config.src || "./images/characters/people/hero.png",
         });
 
         this.behaviorLoop = config.behaviorLoop || [];
